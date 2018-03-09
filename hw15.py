e5aa4adb21779529823d113fa20b1301485be945
ANN <36822439+anyadao@users.noreply.github.com>
2018-03-09 16:38:26 +0200

    Update hw15.py

--- hw15.py
+++ hw15.py
@@ -8,21 +8,12 @@ def circles_intersects(x1, y1, r1, x2, y2, r2):
     min_radius = min(r1, r2)
     max_radius = max(r1, r2)
     radius_sum = r1 + r2
-    if r1 > r2 or r2 > r1:
-        radius_difference = max_radius - min_radius
-        distance = math.sqrt(math.pow(x1 - x2, 2) + math.pow((y1 - y2), 2))
-        if distance == radius_sum or distance == radius_difference or (distance < radius_sum and distance > radius_difference):
-            return True
-        if distance > radius_sum or distance < radius_difference:
-            return False
-    if r1 == r2:
-        radius_sum = r1 + r2
-        distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow((y2 - y1), 2))
-        if distance == radius_sum or distance == 0 or (distance < radius_sum and distance > 0):
-            return True
-        if distance > radius_sum or distance < 0:
-            return False
-
+    radius_difference = max_radius - min_radius
+    distance = math.sqrt(math.pow(x1 - x2, 2) + math.pow((y1 - y2), 2))
+    if distance == radius_sum or distance == radius_difference or (distance < radius_sum and distance > radius_difference):
+        return True
+    if distance > radius_sum or distance < radius_difference:
+        return False
 
 print(circles_intersects(0, 0, 4, 0, 0, 4))
 print(circles_intersects(0, 0, 5, 0, 0, 4))
