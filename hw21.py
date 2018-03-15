d7afdb3fd46912c06bd7f8d89b2a8667788b7c06
ANN <36822439+anyadao@users.noreply.github.com>
2018-03-15 16:51:22 +0200

    Update hw21.py

--- hw21.py
+++ hw21.py
@@ -5,13 +5,11 @@
 import random
 def get_max_digit(big_number):
     print('Случайно сгенерированное 12-тизначное число: ', big_number)
-    str_big_number = str(big_number)
-    number = 0
-    big_number = []
-    for i in range(12):
-        big_number.append(int(str_big_number[i]))
-        if big_number[i] > number:
-            number = big_number[i]
+    big_number_str = str(big_number)
+    number = '0'
+    for num in big_number_str:
+        if num > number:
+            number = num
     return number
 
 big_number = random.randint(100000000000, 999999999999)
