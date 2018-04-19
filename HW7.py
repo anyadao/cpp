ca441cf91e46ed3c205eedbf625184b24f36366e
ANN <36822439+anyadao@users.noreply.github.com>
2018-04-19 12:07:46 +0300

    Delete HW7.py

--- HW7.py
+++
@@ -1,7 +0,0 @@
-#Преобразовать строку с американским форматом даты в европейский. Например, "05.17.2016" преобразуется в "17.05.2016"
-
-american_data = '05.17.2016'
-data_components = american_data.split('.')
-dot = '.'
-european_data = [data_components[1], data_components[0], data_components[2]]
-print (dot.join (european_data))
